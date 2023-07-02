d371e3a4560a1267ae66c322ef90787e913a7582
HARIOM-JHA01 <jhahariom040@gmail.com>
2023-07-02 20:02:22 +0530

    Add project files

---
+++ QR Code Project/index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URl: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    let qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) {
+        console.error("Error writing to file:", err);
+        return;
+      }
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
